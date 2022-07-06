8c989d3b6e38b7e7348cb9b1bad3ca05b4a1bf83
Thinh Nguyen Le Truong <48883771+thinhnglee@users.noreply.github.com>
2022-07-06 17:12:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://rbvh-teampass-backup/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
+aws s3 cp s3://rbvh-teampass-backup/RDM-CodeBuild-Test/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
